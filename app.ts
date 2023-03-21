18192e21395be4273b5f1b580762077752134f5f
Jon Harrell <4829245+jharrell@users.noreply.github.com>
2023-03-21 12:55:41 -0500

    init

---
+++ app.ts
@@ -0,0 +1,69 @@
+import express from 'express';
+
+import prisma from './prisma';
+
+const app = express();
+app.use(express.json())
+
+const PORT = process.env.port || 3000;
+
+// USERS
+app.get('/users', async (_, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+
+// POSTS
+app.get('/posts', async (_, res) => {
+    const posts = await prisma.user.findMany();
+    res.json(posts);
+});
+
+app.get('/posts/:id', async (req, res) => {
+    const { id }: { id?: string } = req.params;
+
+    const post = await prisma.post.findUnique({
+      where: { id: Number(id) },
+    });
+    res.json(post);
+});
+
+app.post('/posts', async (req, res) => {
+    const { title, content, authorEmail } = req.body;
+    const result = await prisma.post.create({
+        data: {
+            title,
+            content,
+            author: { connect: { email: authorEmail } },
+        },
+    });
+    res.json(result);
+});
+
+// PAGES
+app.get('/pages', async (req, res) => {
+    const pages = await prisma.page.findMany();
+    res.json(pages);
+})
+
+app.get('/pages/:id', async (req, res) => {
+    const { id }: { id?: string } = req.params;
+
+    const page = await prisma.page.findUnique({
+      where: { id: Number(id) },
+    });
+    res.json(page);
+})
+
+app.post('/pages', async (req, res) => {
+    const { title, authorEmail } = req.body;
+    const result = await prisma.page.create({
+        data: {
+            title,
+            author: { connect: { email: authorEmail } },
+        },
+    });
+    res.json(result);
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
